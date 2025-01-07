e067334c36e03b560b96cf5210778331abc3250e
jose maldonado <150850527+relic465@users.noreply.github.com>
2025-01-07 16:04:19 +0100

    creando cronometro

--- home/archivo.js
+++ home/archivo.js
@@ -1,3 +1,8 @@
-function Saludo(){
-    swal ('hola a todos!');
+let elementoSegundos = document.getElementById('TiempoElegido').value;
+
+function ComenzarTiempo(){
+    setTimeout(tiempocumplido , 1000*elementoSegundos);
+}
+function tiempocumplido(){
+    alert ('se termino el tiempo')
 }
